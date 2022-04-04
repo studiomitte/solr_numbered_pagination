c74bc64d1f9b102d06a9163f80d5ca8ffb6fdefa
Georg Ringer <georg.ringer@gmail.com>
2022-04-04 10:33:13 +0200

    [TASK] Allow TYPO3 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '10.4.0-11.5.99',
             'solr' => '11.5.0-11.5.99',
             'numbered_pagination' => '1.0.0-1.99.99',
         ],
